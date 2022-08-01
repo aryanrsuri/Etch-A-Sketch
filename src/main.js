313be0cdc13848f6cb5168baa6ae7105536155b8
Aryan <aryanrsuri@gmail.com>
2022-08-01 11:30:54 -0700

    1.0.0 with working board + resize

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const BOX = document.querySelector(".grid");
+function render() {
+    console.log("Hello")
+}
+
+function buildGrid(size) {
+    if (size == null) {
+        size = 16;
+    }
+
+    const CSS = `
+        display: grid;
+        gap: 0.2rem;
+        grid-template-columns: repeat(${size},1fr);
+        grid-template-rows: repeat(${size},1fr);
+    `
+
+    BOX.style.cssText = CSS;
+    for (i=0;i<size*size; i++) {
+        let div = document.createElement("div");
+        BOX.appendChild(div)
+        div.classList.add("gridbox");
+    }
+} 
+
+function resizeGrid(value) {
+    try {
+        BOX.innerHTML = ``;
+        buildGrid(value);
+    } catch(Error) {
+        console.log(Error);
+    }
+
+    return 
+}
+(function() {buildGrid();})();
